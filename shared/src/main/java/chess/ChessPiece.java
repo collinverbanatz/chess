2e734a71662149c55b8c6dedd155c1f92d0ffe1c
collinverbanatz <collindv@byu.edu>
2025-01-15 22:58:32 -0700

    out of bounds error for pawn moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -57,6 +57,9 @@ public class ChessPiece {
         if (type == PieceType.PAWN){
             pawnMovesCalculator(board, myPosition, moves);
         }
+        if (type == PieceType.BISHOP){
+            bishopMovesCalculator(board, myPosition, moves);
+        }
 
 
         return moves;
@@ -75,24 +78,57 @@ public class ChessPiece {
             promotionRow = 1;
         }
 
+//        makes pawn move one step forward
         ChessPosition oneStepForward = new ChessPosition(myPosition.getRow() + direction, myPosition.getColumn());
-        if (oneStepForward.getRow() == promotionRow){
-            for (ChessPiece.PieceType promotion : ChessPiece.PieceType.values()){
-                if (PieceType.PAWN  != promotion && PieceType.KING != promotion){
-                    moves.add(new ChessMove(myPosition, oneStepForward, promotion));
+        ChessPiece team = board.getPiece(oneStepForward);
+        if (team == null) {
+            if (myPosition.getRow() >= 1 && myPosition.getRow() <= 8 && myPosition.getColumn() >= 1 && myPosition.getColumn() <= 8){
+                if (oneStepForward.getRow() == promotionRow) {
+                    for (ChessPiece.PieceType promotion : ChessPiece.PieceType.values()) {
+                        if (PieceType.PAWN != promotion && PieceType.KING != promotion) {
+                            moves.add(new ChessMove(myPosition, oneStepForward, promotion));
+                        }
+                    }
+                }
+                 else {
+                        moves.add(new ChessMove(myPosition, oneStepForward, null));
+                    }
+            }
+        }
+
+//        makes pawn move 2 steps forward if it's at starting position
+        if (team == null){
+            if (myPosition.getRow() >= 1 && myPosition.getRow() <= 8 && myPosition.getColumn() >= 1 && myPosition.getColumn() <= 8){
+
+                if(pieceColor == ChessGame.TeamColor.WHITE && myPosition.getRow() == 2 || pieceColor == ChessGame.TeamColor.BLACK && myPosition.getRow() == 7) {
+                    ChessPosition twoStepForward = new ChessPosition(myPosition.getRow() + direction * 2, myPosition.getColumn());
+                    moves.add(new ChessMove(myPosition, twoStepForward, null));
                 }
             }
         }
-        else{
-            moves.add(new ChessMove(myPosition, oneStepForward, null));
+//        capture a piece if adjacent
+        int[] captureColumns = {-1,1};
+        for (int adjacent : captureColumns){
+            ChessPosition capture = new ChessPosition(myPosition.getRow() + direction, myPosition.getColumn() + adjacent);
+//            checks for valid move
+            if (myPosition.getRow() >= 1 && myPosition.getRow() <= 8 && myPosition.getColumn() >= 1 && myPosition.getColumn() <= 8){
+                ChessPiece enemy = board.getPiece(capture);
+                if (enemy != null && enemy.getTeamColor() != pieceColor){
+                    if (capture.getRow() == promotionRow){
+                        for (ChessPiece.PieceType promotion : ChessPiece.PieceType.values()){
+                            if (PieceType.PAWN  != promotion && PieceType.KING != promotion){
+                                moves.add(new ChessMove(myPosition, capture, promotion));
+                            }
+                        }
+                    }
+                    else{
+                        moves.add(new ChessMove(myPosition, capture, null));}
+                }
+            }
         }
 
-        if(pieceColor == ChessGame.TeamColor.WHITE && myPosition.getRow() == 2 || pieceColor == ChessGame.TeamColor.BLACK && myPosition.getRow() == 7){
-            ChessPosition twoStepForward = new ChessPosition(myPosition.getRow() + direction * 2, myPosition.getColumn());
-            moves.add(new ChessMove(myPosition, twoStepForward, null));
-        }
+    }
+    private void bishopMovesCalculator(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves){
 
 
     }
-
-}
