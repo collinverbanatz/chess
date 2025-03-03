3a607831b6b3b60d281088b5447981dad130ca3c
collinverbanatz <collindv@byu.edu>
2025-03-03 14:03:43 -0700

    duplicate code fix for king and knight

--- shared/src/main/java/chess/movescalculator/KingMovesCalculator.java
+++ shared/src/main/java/chess/movescalculator/KingMovesCalculator.java
@@ -8,28 +8,7 @@ import java.util.Collection;
 public class KingMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves = new ArrayList<>();
         int[][] directions = {{1, 0}, {0, -1}, {0, 1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-        for (int[] direction : directions) {
-            int row = myPosition.getRow();
-            int col = myPosition.getColumn();
-            ChessGame.TeamColor pieceColor = board.getPiece(myPosition).getTeamColor();
-
-            row += direction[0];
-            col += direction[1];
-
-            if (row >= 1 && row <= 8 && col >= 1 && col <= 8) {
-                ChessPosition newPosition = new ChessPosition(row, col);
-                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
-                if (pieceAtNewPosition == null) {
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                }
-                else if (pieceAtNewPosition.getTeamColor() != pieceColor) {
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                }
-            }
-
-        }
-        return moves;
+        return kingKnightMovesCalc(board, myPosition, directions);
     }
 }
--- shared/src/main/java/chess/movescalculator/PieceMovesCalculator.java
+++ shared/src/main/java/chess/movescalculator/PieceMovesCalculator.java
@@ -8,6 +8,31 @@ import java.util.Collection;
 public interface PieceMovesCalculator {
     Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
 
+    default Collection<ChessMove> kingKnightMovesCalc(ChessBoard board, ChessPosition myPosition, int[][] directions) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        for (int[] direction : directions) {
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+            ChessGame.TeamColor pieceColor = board.getPiece(myPosition).getTeamColor();
+
+            row += direction[0];
+            col += direction[1];
+
+            if (row >= 1 && row <= 8 && col >= 1 && col <= 8) {
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+                if (pieceAtNewPosition == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+                else if (pieceAtNewPosition.getTeamColor() != pieceColor) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+            }
+
+        }
+        return moves;
+    }
+
 
     default Collection<ChessMove> infiniteMoves(ChessBoard board, ChessPosition myPosition, int[][] directions) {
         Collection<ChessMove> moves = new ArrayList<>();
