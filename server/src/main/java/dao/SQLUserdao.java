9882693f4017b5c71e608847096a6fd6ff1d4127
collinverbanatz <collindv@byu.edu>
2025-03-06 11:44:57 -0700

    created executeUpdate method

--- server/src/main/java/dao/SQLUserdao.java
+++ server/src/main/java/dao/SQLUserdao.java
@@ -3,12 +3,39 @@ package dao;
 import dataaccess.DataAccessException;
 import models.UserData;
 
+import javax.xml.crypto.Data;
 import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class SQLUserdao implements Usrdao{
 
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
 
-    public SQLUserdao() {
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new DataAccessException("DataBase Query failed");
+        }
+    }
+
+
+    public SQLUserdao() throws DataAccessException {
         try {
             DatabaseManager.createDatabase();
         } catch (DataAccessException e) {
@@ -21,21 +48,17 @@ public class SQLUserdao implements Usrdao{
                 }
             }
         } catch (SQLException | DataAccessException e) {
-            try {
-                throw new DataAccessException("wrong");
-            } catch (DataAccessException ex) {
-                throw new RuntimeException(ex);
-            }
+            throw new DataAccessException("could not connect to Database");
         }
     }
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS  pet (
+            CREATE TABLE IF NOT EXISTS  users (
               `username` varchar(255) NOT NULL,
-              'password' varchar(255) NOT NULL,
-              'email' varchar(255) NOT NULL,
-              PRIMARY KEY (`username`),
+              `password` varchar(255) NOT NULL,
+              `email` varchar(255) NOT NULL,
+              PRIMARY KEY (`username`)
             )"""
     };
 
